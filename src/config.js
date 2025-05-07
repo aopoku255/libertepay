e38b031dfc1fc0459125726a6db9e8e636522cab
aopoku255 <aopoku255@gmail.com>
2025-05-07 21:23:14 +0000

    first commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+  api: {
+    API_URL: "https://360pay.deduceinc.com/api",
+    // API_URL: "https://api-node.themesbrand.website",
+  },
+};
